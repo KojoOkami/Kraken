a2fb3fd924ac166bf60c2c8f02168ba352af00aa
Connor Mills <connormills535@gmail.com>
2020-03-04 15:09:05 -0700

    Combined ComputerFile and LoadFiles

--- src/krakenwriter/backend/ComputerFile.java
+++ src/krakenwriter/backend/ComputerFile.java
@@ -1,10 +1,11 @@
 package krakenwriter.backend;
 
+import java.util.ArrayList;
+
 public class ComputerFile {
 
     private String PATH;
     public Object object;
-    public String saveStatus;
 
     public ComputerFile(Obj object) {
         this.PATH = "..\\" + VisualSpace.projectName.replaceAll(" ", "") + "\\" + object.title.strip().replaceAll(" ", "_") + ".txt";
@@ -20,13 +21,18 @@ public class ComputerFile {
     public void saveDocument() {
         
     }
+    
+    public static String saveStatus; //This will be updated constantly in save() to be used by VisualSpace to tell what stage of saving the program is at
 
-    public Object getText() {
-
+    public static ArrayList<Object>[] load(String projectName) {
+        //Will find all of the files for the specified project
+        //All objects found in the data will be parsed into the respective object type and returned in an ArrayList
         return null;
     }
 
-    public void saveText() {
-
+    public static void save(String projectName, ArrayList<Object>[] objects) {
+        //Will find the location for the files for the specified project if it exists
+        //Will create or overwrite the data for each object in the ArrayList
+        //Uses ComputerFile.getData() to get the data and parse it
     }
 }
