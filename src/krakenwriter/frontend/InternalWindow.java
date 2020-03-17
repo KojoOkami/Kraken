640ca4684cf15f7ce8e0ab1803b6bf7ede2ba80b
Connor Mills <connormills535@gmail.com>
2020-03-16 18:11:18 -0600

    incomplete

--- src/krakenwriter/frontend/InternalWindow.java
+++ src/krakenwriter/frontend/InternalWindow.java
@@ -1,20 +1,15 @@
 package krakenwriter.frontend;
 
+import java.awt.Color;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Image;
-import java.awt.MouseInfo;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.util.HashSet;
-import java.util.Set;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JInternalFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.event.InternalFrameEvent;
@@ -29,8 +24,8 @@ public class InternalWindow extends JInternalFrame implements InternalFrameListe
 
     JPanel pane;
 
-    JLabel name;
-    JLabel description;
+    JTextField name;
+    JTextField description;
     JButton openBtn;
     JButton editBtn;
 
@@ -43,45 +38,52 @@ public class InternalWindow extends JInternalFrame implements InternalFrameListe
                 true, //maximizable
                 true);
         this.obj = object;
-        
+
         initComponents();
     }
 
     private void initComponents() {
         pane = new JPanel();
+        pane.setBackground(Color.white);
 
         pane.setLayout(new GridBagLayout());
         GridBagConstraints c;
 
-        name = new JLabel(obj.title);
-        name.addMouseListener(l);
+        name = new JTextField();
+        name.setText(obj.title);
+        name.setHorizontalAlignment(JTextField.CENTER);
+        name.setBackground(null);
+        name.setBorder(null);
         c = new GridBagConstraints();
-        c.gridx = 1;
+        c.gridx = 0;
         c.gridy = 0;
-        //c.gridwidth = 1;
+        c.gridwidth = 3;
         c.gridheight = 1;
-        c.fill = GridBagConstraints.HORIZONTAL;
-        //c.ipadx = 0;
-        c.ipady = 0;
-        c.insets = new Insets(0, 0, 0, 0);
-        c.anchor = GridBagConstraints.PAGE_START;
-        //c.weightx = 0;
-        c.weighty = 1.0;
-        pane.add(name, c);
-
-        description = new JLabel(obj.description);
-        description.addMouseListener(l);
-        c.gridx = 1;
-        c.gridy = 1;
-        //c.gridwidth = 1;
-        c.gridheight = 1;
-        c.fill = GridBagConstraints.HORIZONTAL;
-        //c.ipadx = 0;
-        c.ipady = 0;
+        c.fill = GridBagConstraints.BOTH;
+        c.ipadx = 3;
+        c.ipady = 3;
         c.insets = new Insets(0, 0, 0, 0);
         c.anchor = GridBagConstraints.CENTER;
-        //c.weightx = 0;
-        c.weighty = 1.0;
+        c.weightx = 10;
+        //c.weighty = 1;
+        pane.add(name, c);
+
+        description = new JTextField();
+        description.setText(obj.description);
+        description.setHorizontalAlignment(JTextField.CENTER);
+        description.setBackground(null);
+        description.setBorder(null);
+        c.gridx = 0;
+        c.gridy = 1;
+        c.gridwidth = 3;
+        c.gridheight = 1;
+        c.fill = GridBagConstraints.BOTH;
+        c.ipadx = 3;
+        c.ipady = 3;
+        c.insets = new Insets(0, 0, 0, 0);
+        c.anchor = GridBagConstraints.CENTER;
+        c.weightx = 10;
+        //c.weighty = 1;
         pane.add(description, c);
 
         openBtn = new JButton();
@@ -91,36 +93,36 @@ public class InternalWindow extends JInternalFrame implements InternalFrameListe
         c.gridy = 2;
         c.gridwidth = 1;
         //c.gridheight = 1;
-        c.fill = GridBagConstraints.VERTICAL;
+        c.fill = GridBagConstraints.BOTH;
         c.ipadx = 0;
         //c.ipady = 0;
         c.insets = new Insets(0, 0, 0, 0);
         c.anchor = GridBagConstraints.LAST_LINE_START;
-        c.weightx = 1.0;
-        //c.weighty = 0;
+        c.weightx = 1;
+        c.weighty = 10;
         pane.add(openBtn, c);
-        
+
         editBtn = new JButton();
         ImageIcon imageIcon = new ImageIcon("./bin/edit.png");
         Image image = imageIcon.getImage();
-        Image newimg = image.getScaledInstance(15, 15,  java.awt.Image.SCALE_SMOOTH); 
+        Image newimg = image.getScaledInstance(15, 15, java.awt.Image.SCALE_SMOOTH);
         editBtn.setIcon(new ImageIcon(newimg));
         openBtn.addActionListener(new EditBtnListener());
         c.gridx = 2;
         c.gridy = 2;
         c.gridwidth = 1;
         //c.gridheight = 1;
-        c.fill = GridBagConstraints.VERTICAL;
+        c.fill = GridBagConstraints.BOTH;
         c.ipadx = 0;
         //c.ipady = 0;
         c.insets = new Insets(0, 0, 0, 0);
         c.anchor = GridBagConstraints.LAST_LINE_END;
-        c.weightx = 1.0;
-        //c.weighty = 0;
+        c.weightx = 1;
+        c.weighty = 10;
         pane.add(editBtn, c);
 
         add(pane);
-        
+
         this.pack();
     }
 
@@ -157,46 +159,6 @@ public class InternalWindow extends JInternalFrame implements InternalFrameListe
     @Override
     public void internalFrameDeactivated(InternalFrameEvent e) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-    private class editTextListener implements MouseListener {
-
-        JLabel label;
-        JTextField field;
-        
-        private boolean singleClicked;
-        
-        public editTextListener(JLabel label, JTextField field) {
-            this.label = label;
-            this.field = field;
-            this.singleClicked = false;
-        }
-        
-        @Override
-        public void mouseClicked(MouseEvent e) {
-        }
-
-        @Override
-        public void mousePressed(MouseEvent e) {
-        }
-
-        @Override
-        public void mouseReleased(MouseEvent e) {
-            if (singleClicked) {
-                label.setVisible(false);
-                field.setEnabled(true);
-                field.setVisible(true);
-            }
-        }
-
-        @Override
-        public void mouseEntered(MouseEvent e) {
-        }
-
-        @Override
-        public void mouseExited(MouseEvent e) {
-        }
-        
     }
 
     private class OpenBtnListener implements ActionListener {
